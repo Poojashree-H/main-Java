6dfd65109d02c1e0899b0d7601edd9029dd1af9c
Poojashree-H <poojashreep267@gmail.com>
2025-06-17 19:32:52 +0530

    sop

---
+++ Footwear.java
@@ -0,0 +1,9 @@
+class FootWear{
+public static void main(String args[]){
+System.out.println("Material:SYNTHETIC");
+System.out.println("Lifestyle:Casual");
+System.out.println("Heel Type:Wedges");
+System.out.println("Heel Height:5 Inch");
+System.out.println("Care Instructions: Rotate your pair of shoes once every other day, allowing them to de-odorize and retain their shapes.Use Shoe bags to prevent any stains or mildew");
+}
+}
